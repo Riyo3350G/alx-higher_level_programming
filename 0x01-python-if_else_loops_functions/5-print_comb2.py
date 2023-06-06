2d21a5496fb3e7a998b06e24dc1058b00a580f09
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-06-06 13:44:41 +0100

    Task 5 output fix

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for digit in range(0, 100):
-    print("{:02d}, ".format(digit), end="")
     if digit == 99:
         print("{0:02d}".format(digit), end="\n")
+    print("{:02d}, ".format(digit), end="")
+    
