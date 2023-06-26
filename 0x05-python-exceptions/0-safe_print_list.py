eff22cca020b708e64f02e40c0860f41721a88bf
riyo3350g <yechouben@gmail.com>
2023-06-26 04:13:58 -0700

    Task 0 update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 def safe_print_list(my_list=[], x=0):
     ctr = 0
 
-    for index in range(x):
+    for index in range(0, x):
         try:
-            print(my_list[index], end="")
+            print(f"{my_list[index]}", end="")
             ctr += 1
         except IndexError:
             break
     print()
-    return ctr
+    return (ctr)
