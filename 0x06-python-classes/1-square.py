6cf717791108253f1548976f674607d28a132f60
riyo3350g <yechouben@gmail.com>
2023-06-27 08:01:49 -0700

    add task 0

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" class that defines a square"""
+
+
+class Square:
+    """ class that defines a square"""
+    def __init__(self, size):
+        """initialization of square
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size  #: size of the square
