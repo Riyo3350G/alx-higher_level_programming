a68f781905a0f20305b27f260040895db541133d
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-07-04 01:42:37 +0100

    Task 8 added

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -47,14 +47,14 @@ class Rectangle:
         return ((self.__width * 2) + (self.__height * 2))
 
     @staticmethod
-    def bigger_or_equal(rec1, rec2):
-        if not isinstance(rec1, Rectangle):
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rec2, Rectangle):
+        if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() >= rec2.area():
-            return (rec1)
-        return (rec2)
+        if rect_1.area() >= rect_2.area():
+            return (rect_1)
+        return (rect_2)
 
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
