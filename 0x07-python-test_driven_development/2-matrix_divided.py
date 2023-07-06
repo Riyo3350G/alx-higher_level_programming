7435c4ab747c67ee2d77388673729acfc573478b
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-07-06 22:03:50 +0100

    Style fix 0

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -9,12 +9,12 @@ def matrix_divided(matrix, div):
         matrix (list): List of lists of ints or floats.
         div (int/float): The divider.
     Raises:
-        TypeError: If the matrix contains non-numbers.
+        TypeError: If the matrix contains not int/floats.
         TypeError: If the matrix contains rows of different sizes.
-        TypeError: If div is not an int or float.
-        ZeroDivisionError: If div is 0.
+        TypeError: If divider is not an int or  a float.
+        ZeroDivisionError: If divider equal to 0.
     Returns:
-        A new matrix representing the result of the division.
+        A new matrix result of the division.
     """
     if (not isinstance(matrix, list) or matrix == [] or
             not all(isinstance(row, list) for row in matrix) or
