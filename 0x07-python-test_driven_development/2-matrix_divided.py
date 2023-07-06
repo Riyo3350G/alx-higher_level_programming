93c76e981bb97f07fb17ba9022e28432c58eed04
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-07-06 21:47:47 +0100

    Fix 1

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
-"""
-This module divides all elements of a matrix
-"""
+"""Defines a matrix division function."""
 
 
 def matrix_divided(matrix, div):
-    """
-    This function divide all matrix elements by div
-    matrix must be a list of lists of integers or floats.
+    """Divide all elements of a matrix.
+
+    Args:
+        matrix (list): A list of lists of ints or floats.
+        div (int/float): The divisor.
+    Raises:
+        TypeError: If the matrix contains non-numbers.
+        TypeError: If the matrix contains rows of different sizes.
+        TypeError: If div is not an int or float.
+        ZeroDivisionError: If div is 0.
+    Returns:
+        A new matrix representing the result of the division.
     """
     if (not isinstance(matrix, list) or matrix == [] or
             not all(isinstance(row, list) for row in matrix) or
@@ -24,4 +31,5 @@ def matrix_divided(matrix, div):
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    return ([list(map(lambda x: round(x/div, 2), row)) for row in matrix])
+
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
