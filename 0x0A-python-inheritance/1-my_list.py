0acb4e4e1fe00f10aeb27b23f5154c1ec0ac354b
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-07-10 17:41:35 +0100

    Try fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""
+mod 1-my_list contains class MyList
+"""
+
 
 class MyList(list):
+    """ define Mylist Class """
+
+    def __init__(self):
+        """ init the object """
+        super().__init__()
 
     def print_sorted(self):
-        return (sorted(self))
+        """ prints sorted list """
+        print(sorted(self))
