2eff72276ed180882b104f0ced694ed5c1a0f90a
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-07-11 16:30:23 +0100

    Task 1 added

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""write_file module"""
+
+def write_file(filename="", text=""):
+    """func writes a string to file and return n of chars written"""
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
