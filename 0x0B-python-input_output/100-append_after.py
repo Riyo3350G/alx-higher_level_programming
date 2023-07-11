d648c5ed9eb13359ba705ab84bdd47ba2983d4e6
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-07-11 23:32:28 +0100

    Task 100 added

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""append_after module"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """inserts a line of text to a file,
+    after each line containing a specific string"""
+    insrtd_text = ""
+    with open(filename) as f:
+        for line in f:
+            insrtd_text += line
+            if search_string in line:
+                insrtd_text += new_string
+    with open(filename, "a") as r:
+        r.write(insrtd_text)
