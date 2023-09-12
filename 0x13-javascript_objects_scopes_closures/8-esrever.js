20d52b2787bab90d9fb4ca24c6f71964c3c45194
riyo3350g <yechouben@gamail.com>
2023-09-12 20:20:51 +0100

    Task 8 added

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  let i = list.length - 1;
+  const newList = [];
+
+  while (i >= 0) {
+    newList.push(list[i]);
+    i--;
+  }
+
+  return newList;
+};
