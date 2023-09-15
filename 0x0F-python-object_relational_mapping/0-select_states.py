3a6740317a21e54ca25a3d932b68407c1d861823
root <root@DESKTOP-U2066LF.localdomain>
2023-09-15 17:01:34 +0100

    Task 0 added

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    connc = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                            passwd=sys.argv[2], db=sys.argv[3])
+    cur = connc.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    row = cur.fetchall()
+    for state in row:
+        print(state)
+    cur.close()
+    connc.close()
