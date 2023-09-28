abfda653e65322980f65a3781d1d6c27c248f6d3
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-09-29 00:35:40 +0100

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Script that makes a request to 0.0.0.0:5000/catch_me 
 # that causes the server to respond with a message containing You got me!, in the body of the response.
-curl -sL 0.0.0.0:5000/catch_me -d "user_id=98" -X PUT -H "Origin: School"
+# curl -sL 0.0.0.0:5000/catch_me -d "user_id=98" -X PUT -H "Origin:School"
+curl -sLX PUT -H "Origin:School" -d "user_id=98" "0.0.0.0:5000/catch_me"
