518d5458ff68ea1f60f0da03c857165a7f07287d
riyo3350g <yechouben@gamail.com>
2023-09-28 17:22:39 +0100

    Task 0 added

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 # script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 
-curl -s "$1" | wc -c
+url="$1"
+
+# Use curl to fetch the content of the URL
+response=$(curl -s "$url")
+
+# Use wc -c to count the number of characters in the response(body size)
+body_size=$(echo -n "$response" | wc -c)
+# Print it
+echo "$body_size"
