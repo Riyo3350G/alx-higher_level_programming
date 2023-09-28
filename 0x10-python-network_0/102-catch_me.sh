7278e1cc33ad754e2845a4dcf8831366ffe46376
riyo3350G <yechouben@gmail.com>
2023-09-28 16:41:53 -0700

    Task 102 error

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Script that makes a request to 0.0.0.0:5000/catch_me 
 # that causes the server to respond with a message containing You got me!, in the body of the response.
-# curl -sL 0.0.0.0:5000/catch_me -d "user_id=98" -X PUT -H "Origin:School"
-curl -sLX PUT -H "Origin:HoolbertonSchool" -d "user_id=98" "0.0.0.0:5000/catch_me"
+curl -s -L 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin:School"
