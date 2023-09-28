9ddb8fc42d80927c4ab59435462a5e6b8ed98cb9
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-09-28 19:32:09 +0100

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,6 +4,18 @@
 
 def find_peak(list_of_integers):
     """Find a peak in a list"""
-    if list_of_integers:
-        list_of_integers.sort()
-        return list_of_integers[len(list_of_integers) - 1]
+    if list_of_integers = []:
+        return None
+
+    left = 0
+    right = len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            right = mid
+        else:
+            left = mid + 1
+
+    return list_of_integers[left]
