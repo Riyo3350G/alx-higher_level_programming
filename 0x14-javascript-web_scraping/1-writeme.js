fa645903472cb8c930d7d62894a07ef12fd158be
riyo3350g <yechouben@gamail.com>
2023-10-24 14:54:25 +0100

    Task 1 style fix

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
-const file_path = process.argv[2];
+const filePath = process.argv[2];
 const content = process.argv[3];
 
-fs.writeFileSync(file_path, content, 'utf8', function (err) {
+fs.writeFileSync(filePath, content, 'utf8', function (err) {
   if (err) {
     console.log(err);
   }
